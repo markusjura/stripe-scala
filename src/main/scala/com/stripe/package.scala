d7c098b863e68886a501b1a11b3d119300051e6e
Markus Jura <markus.jura@googlemail.com>
2013-04-27 16:11:02 -0700

    publish to gh-pages
    Signed-off-by: Markus Jura <markus.jura@googlemail.com>

--- src/main/scala/com/stripe/package.scala
+++
@@ -1,8 +0,0 @@
-package com
-
-import org.apache.http.conn.ClientConnectionManager
-
-package object stripe {
-    var apiKey: String = ""
-    var connectionManager: ClientConnectionManager = null
-}
